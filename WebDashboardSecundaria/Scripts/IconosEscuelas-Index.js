2f0d89acaa3ef229deef993de7c71263b5394f4c
ROdrigoRRRRD <roddrigo2003cod@gmail.com>
2023-10-12 19:48:36 -0600

    12/10

---
+++ WebDashboardSecundaria/Scripts/IconosEscuelas-Index.js
@@ -0,0 +1,35 @@
+﻿var add = document.querySelector(".icon-add-es");
+var edits = document.querySelectorAll(".icon-edit-es");
+var datails = document.querySelectorAll(".icon-details-es");
+var delates = document.querySelectorAll(".icon-delate-es");
+var dotsElements = document.querySelectorAll(".icon-menu_item");
+
+
+
+add.innerHTML = `<div class="app__secundaria-add">
+            <span class="icon-add-content">
+                <i class="fa-solid fa-plus"></i>
+            </span >
+        </div>`;
+
+
+add.style.textDecoration = "none"
+
+edits.forEach((edit) => {
+    edit.innerHTML = `<p>Editar</p> <span><i class="fa-solid fa-pen"></i></span>`;
+})
+
+datails.forEach((detail) => {
+    detail.innerHTML = `<p>Detalles</p> <span><i class="fa-solid fa-magnifying-glass"></i></span>`
+})
+delates.forEach((delate) => {
+    delate.innerHTML = `<p>Eliminar</p> <span><i class="fa-solid fa-trash"></i></span>`
+})
+
+dotsElements.forEach((dotsElement) => {
+    dotsElement.addEventListener('click', () => {
+        var menu = dotsElement.closest(".app__secundaria-item").querySelector(".app__secundaria-item_menu");
+
+        menu.classList.toggle("view");
+    });
+});
---
+++ WebDashboardSecundaria/Scripts/IconsDashboard.js
@@ -0,0 +1,29 @@
+﻿var escuela = document.querySelector(".icon-Escuela-DBD");
+var alumnos = document.querySelector(".icon-Alumnos-DBD");
+var cicloEscolar = document.querySelector(".icon-CicloEscolar-DBD");
+var grupoAlumnos = document.querySelector(".icon-GrupoAlumnos-DBD");
+var largeData = document.querySelector(".icon-LargeData-DBD");
+var grupos = document.querySelector(".icon-Grupos-DBD");
+var grados = document.querySelector(".icon-Grados-DBD");
+var materiaGrados = document.querySelector(".icon-MateriaGrados-DBD");
+var materias = document.querySelector(".icon-Materias-DBD");
+var orientadores = document.querySelector(".icon-Orientadores-DBD");
+var profesores = document.querySelector(".icon-Profesores-DBD");
+var tutorAlumnos = document.querySelector(".icon-TutorAlumnos-DBD");
+var tutores = document.querySelector(".icon-Tutores-DBD");
+
+escuela.innerHTML = `<span class="icon-nav"><i class="fa-solid fa-school"></i></span>`
+alumnos.innerHTML = `<span class="icon-nav"><i class="fa-solid fa-address-book"></i></span>`
+cicloEscolar.innerHTML = `<span class="icon-nav"><i class="fa-solid fa-arrows-spin"></i></span>`
+grupoAlumnos.innerHTML = `<span class="icon-nav"><i class="fa-solid fa-users-viewfinder"></i></span>`
+largeData.innerHTML = `<span class="icon-nav"><i class="fa-solid fa-link"></i></span>`
+grupos.innerHTML = `<span class="icon-nav"><i class="fa-solid fa-layer-group"></i></span>`
+grados.innerHTML = `<span class="icon-nav"><i class="fa-regular fa-star"></i></span>`
+grados.innerHTML = `<span class="icon-nav"><i class="fa-regular fa-star"></i></span>`
+materiaGrados.innerHTML = `<span class="icon-nav"><i class="fa-solid fa-book-atlas"></i></span>`
+materias.innerHTML = `<span class="icon-nav"><i class="fa-solid fa-book-bookmark"></i></span>`
+orientadores.innerHTML = `<span class="icon-nav"><i class="fa-solid fa-compass"></i></span>`
+profesores.innerHTML = `<span class="icon-nav"><i class="fa-solid fa-chalkboard-user"></i></span>`
+tutorAlumnos.innerHTML = `<span class="icon-nav"><i class="fa-solid fa-people-group"></i></span>`
+tutores.innerHTML = `<span class="icon-nav"><i class="fa-solid fa-people-roof"></i></span>`
+
